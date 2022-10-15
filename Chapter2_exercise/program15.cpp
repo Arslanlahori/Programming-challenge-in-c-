ab776d19af0f3c09a482f96d323a1f2cde246477
arslanlahori <arslanansari786.iu@gmail.com>
2022-10-15 08:10:18 +0500

    Triangle Pattern

---
+++ Chapter2_exercise/program15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/*
+Triangle Pattern
+Write a program that displays the following pattern on the screen:
+ *
+ ***
+ *****
+ *******
+ */
+int main()
+{
+    cout << "*" << endl;
+    cout << "***" << endl;
+    cout << "*****" << endl;
+    cout << "*******" << endl;
+    return 0;
+}
