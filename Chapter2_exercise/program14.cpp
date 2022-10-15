fbf26ff3666e6bd3e54cee32c7d58606dec4b3db
arslanlahori <arslanansari786.iu@gmail.com>
2022-10-15 08:08:51 +0500

    Personal Information Write a program that displays the following pieces of information, each on a separate line: your name Your address, with city, state, and ZIP code Your address, with city, state, and ZIP code

---
+++ Chapter2_exercise/program14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+Personal Information
+Write a program that displays the following pieces of information, each on a separate line:
+Your name
+Your address, with city, state, and ZIP code
+Your telephone number
+Your college major
+Use only a single cout statement to display all of this information
+*/
+int main()
+{
+    cout << "My name is Arslan" << endl
+         << "Khursheed town ,Mian channu,punjab,90900" << endl
+         << "0347425363590" << endl
+         << "degree college" << endl;
+    return 0;
+}
