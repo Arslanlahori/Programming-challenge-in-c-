07550d54338d49495b68026a5a35b26a4c7c543e
arslanlahori <arslanansari786.iu@gmail.com>
2022-10-15 08:11:04 +0500

    Triangle Pattern

---
+++ Chapter2_exercise/program16.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+       Diamond Pattern
+Write a program that displays the following pattern:
+    *
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+*/
+int main()
+{
+    cout << "    *   " << endl;
+    cout << "   ***   " << endl;
+    cout << "  *****   " << endl;
+    cout << " *******   " << endl;
+    cout << "  *****   " << endl;
+    cout << "   ***   " << endl;
+    cout << "    *   " << endl;
+    return 0;
+}
