0f6e84e0ccf7f8cfa7cfd9f2e63b212dc3a54de3
arslanlahori <arslanansari786.iu@gmail.com>
2022-10-15 07:34:17 +0500

    Sum of two numbers,Write a program that stores the integers 50 and 100 in variables, and stores the sum of and these two in a variable named total

---
+++ Chapter2_exercise/program1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+// 1. Sum of Two Numbers
+// Write a program that stores the integers 50 and 100 in variables, and stores the sum of
+// these two in a variable named total .
+int main()
+{
+    int a = 50;
+    int b = 100;
+    int total;
+    total = a + b;
+    cout << "the sum of thses two varibales =" << total << endl;
+    return 0;
+}
